b86923b2c189093ff3a7ceb4ac4b801296364c8e
gehaifu <16964544@qq.com>
2025-11-20 13:43:40 +0800

    init

--- src/UniswapV3Factory.sol
+++ src/UniswapV3Factory.sol
@@ -20,25 +20,18 @@ contract UniswapV3Factory is IUniswapV3PoolDeployer {
     PoolParameters public parameters;
 
     mapping(uint24 => uint24) public fees;
-    mapping(address => mapping(address => mapping(uint24 => address)))
-        public pools;
+    mapping(address => mapping(address => mapping(uint24 => address))) public pools;
 
     constructor() {
         fees[500] = 10;
         fees[3000] = 60;
     }
 
-    function createPool(
-        address tokenX,
-        address tokenY,
-        uint24 fee
-    ) public returns (address pool) {
+    function createPool(address tokenX,address tokenY,uint24 fee) public returns (address pool) {
         if (tokenX == tokenY) revert TokensMustBeDifferent();
         if (fees[fee] == 0) revert UnsupportedFee();
 
-        (tokenX, tokenY) = tokenX < tokenY
-            ? (tokenX, tokenY)
-            : (tokenY, tokenX);
+        (tokenX, tokenY) = tokenX < tokenY ? (tokenX, tokenY) : (tokenY, tokenX);
 
         if (tokenX == address(0)) revert ZeroAddressNotAllowed();
         if (pools[tokenX][tokenY][fee] != address(0))
@@ -52,11 +45,7 @@ contract UniswapV3Factory is IUniswapV3PoolDeployer {
             fee: fee
         });
 
-        pool = address(
-            new UniswapV3Pool{
-                salt: keccak256(abi.encodePacked(tokenX, tokenY, fee))
-            }()
-        );
+        pool = address(new UniswapV3Pool{salt: keccak256(abi.encodePacked(tokenX, tokenY, fee))}());
 
         delete parameters;
 
